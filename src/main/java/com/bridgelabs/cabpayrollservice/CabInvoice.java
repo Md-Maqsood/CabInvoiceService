26d404591ffd0eb20def8e18ada8ba4fc1519e1a
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-24 09:43:08 +0530

    MaheswariKoyyani : Refractor the code to calculate the fare

--- src/main/java/com/bridgelabs/cabpayrollservice/CabInvoice.java
+++ src/main/java/com/bridgelabs/cabpayrollservice/CabInvoice.java
@@ -5,14 +5,10 @@ import org.apache.logging.log4j.Logger;
 
 public class CabInvoice {
 	private static final Logger logger = LogManager.getLogger(CabInvoice.class);
-	public static final double MINIMUM_COST_PER_KILOMETER =10.0;
-	public static final int COST_PER_TIME=1;
-
-	public static void main(String[] args) {
-		logger.info("Welcome to cab payroll service program");
-	}
+	public static final double MINIMUM_COST_PER_KILOMETER = 10.0;
+	public static final int COST_PER_TIME = 1;
 
 	public double calculateFare(double distance, int time) {
-		return distance*MINIMUM_COST_PER_KILOMETER + time*COST_PER_TIME;
+		return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
 	}
 }
