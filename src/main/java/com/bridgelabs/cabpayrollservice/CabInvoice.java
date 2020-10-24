304c42f828cdd831dc3313223b5c263c2cb29581
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-10-24 10:28:31 +0530

    MaheswariKoyyani : Refactor for multiple inputs

--- src/main/java/com/bridgelabs/cabpayrollservice/CabInvoice.java
+++ src/main/java/com/bridgelabs/cabpayrollservice/CabInvoice.java
@@ -17,6 +17,10 @@ public class CabInvoice {
 			}
 	}
 	public double calculateFare(Ride[] rides) {
-		return 0.0;
+		double totalFare=0;
+		for(Ride ride:rides) {
+			totalFare+=this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
 	}
 }
--- src/main/java/com/bridgelabs/cabpayrollservice/Ride.java
+++ src/main/java/com/bridgelabs/cabpayrollservice/Ride.java
@@ -1,6 +1,10 @@
 package com.bridgelabs.cabpayrollservice;
 
 public class Ride {
+	public double distance;
+	public int time;
 	public Ride(double distance,int time) {
+		this.distance=distance;
+		this.time=time;
 	}
 }
