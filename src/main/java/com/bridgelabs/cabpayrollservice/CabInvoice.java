0460f8f374a11ab759f93832efe9bbd618aa6f4e
maqsood <maqsoodhamd37@gmail.com>
2020-10-24 10:03:11 +0530

    [Maqsood].update: added the feature to return minimum fare if total fare is less than minimum fare

--- src/main/java/com/bridgelabs/cabpayrollservice/CabInvoice.java
+++ src/main/java/com/bridgelabs/cabpayrollservice/CabInvoice.java
@@ -7,9 +7,13 @@ public class CabInvoice {
 	private static final Logger logger = LogManager.getLogger(CabInvoice.class);
 	public static final double MINIMUM_COST_PER_KILOMETER = 10.0;
 	public static final int COST_PER_TIME = 1;
-	
+	public static final double MINIMUM_FARE=5.0;
 	public double calculateFare(double distance, int time) {
 		double totalFare= distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-			return totalFare;
+			if(totalFare<MINIMUM_FARE) {
+				return MINIMUM_FARE;
+			}else {
+				return totalFare;
+			}
 	}
 }
--- src/test/java/com/bridgelabs/cabpayrollservice/CabPayrollTest.java
+++ src/test/java/com/bridgelabs/cabpayrollservice/CabPayrollTest.java
@@ -17,7 +17,7 @@ public class CabPayrollTest {
 	public void givenLessDistanceOrTimeShouldReturnMinFare() {
 		CabInvoice cabInvoice=new CabInvoice();
 		double distance=0.1;
-		int time=5;
+		int time=1;
 		double fare=cabInvoice.calculateFare(distance,time);
 		Assert.assertEquals(5, fare, 0.0);
 	}
